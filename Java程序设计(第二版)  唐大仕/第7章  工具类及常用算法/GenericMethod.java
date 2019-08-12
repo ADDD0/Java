d7b62943764472af28abb0412e87b6d7941d8cd0
ADDD0 <448417505@qq.com>
2019-08-12 20:52:07 +0800

    20:50 2019/8/12

---
+++ Java程序设计(第二版)  唐大仕/第7章  工具类及常用算法/GenericMethod.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class GenericMethod {
+	public static void main(String[] args) {
+		Date date = BeanUtil.<Date>getInstance("java.util.Date");
+		System.out.println(date);
+	}
+}
+
+class BeanUtil {
+	public static <T> T getInstance(String clzName) {
+		try {
+			Class c = Class.forName(clzName);
+			return (T) c.newInstance();
+		} catch (ClassNotFoundException ex) {
+		} catch (InstantiationException ex) {
+		} catch (IllegalAccessException ex) {
+		}
+		return null;
+	}
+}
