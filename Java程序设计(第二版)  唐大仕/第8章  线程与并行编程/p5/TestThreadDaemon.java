f8fce86206ee5f905a494c5863e8d926c7de19e6
ADDD0 <448417505@qq.com>
2019-08-18 10:13:17 +0800

    10:10 2019/8/18

---
+++ Java程序设计(第二版)  唐大仕/第8章  线程与并行编程/p5/TestThreadDaemon.java
@@ -0,0 +1,30 @@
+package p5;
+
+import java.util.*;
+
+public class TestThreadDaemon {
+	public static void main(String[] args) {
+		Thread t = new MyThread();
+		t.setDaemon(true);
+		t.start();
+
+		System.out.println("Main--" + new Date());
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException ex) {
+		}
+		System.out.println("Main End");
+	}
+}
+
+class MyThread extends Thread {
+	public void run() {
+		for (int i = 0; i < 10; ++i) {
+			System.out.println(i + "--" + new Date());
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException ex) {
+			}
+		}
+	}
+}
