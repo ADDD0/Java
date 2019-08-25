b0a84d158732693d5050c2debd6b56f32a9c9f04
ADDD0 <448417505@qq.com>
2019-08-25 22:11:55 +0800

    22:10 2019/8/25

---
+++ Java程序设计(第二版)  唐大仕/第10章  图形用户界面/TextEditorApp.java
@@ -0,0 +1,160 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+
+class TextEditorFrame extends JFrame {
+	File file = null;
+	Color color = Color.black;
+
+	TextEditorFrame() {
+		initTextPane();
+		initMenu();
+		initAboutDialog();
+		initToolBar();
+	}
+
+	// 将文本框放入有滚动对象,并加入到Frame中
+	void initTextPane() {
+		getContentPane().add(new JScrollPane(text));
+	}
+
+	JTextPane text = new JTextPane(); // 文本框
+	JFileChooser filechooser = new JFileChooser(); // 文件选择对话框
+	JColorChooser colorchooser = new JColorChooser(); // 颜色选择对话框
+	JDialog about = new JDialog(this); // 关于对话框
+	JMenuBar menubar = new JMenuBar(); // 菜单
+
+	JMenu[] menus = new JMenu[] { new JMenu("File"), new JMenu("Edit"), new JMenu("Help") };
+	JMenuItem menuitems[][] = new JMenuItem[][] {
+			{ new JMenuItem("New"), new JMenuItem("Open..."), new JMenuItem("Save..."), new JMenuItem("Exit") },
+			{ new JMenuItem("Copy"), new JMenuItem("Cut"), new JMenuItem("Paste"), new JMenuItem("Color...") },
+			{ new JMenuItem("About") } };
+
+	// 初始化菜单
+	void initMenu() {
+		for (int i = 0; i < menus.length; ++i) {
+			menubar.add(menus[i]);
+			for (int j = 0; j < menuitems[i].length; ++j) {
+				menus[i].add(menuitems[i][j]);
+				menuitems[i][j].addActionListener(action);
+			}
+		}
+		this.setJMenuBar(menubar);
+	}
+
+	// 菜单事件处理
+	ActionListener action = new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			JMenuItem mi = (JMenuItem) e.getSource();
+			String id = mi.getText();
+			if (id.equals("New")) {
+				text.setText("");
+				file = null;
+			} else if (id.equals("Open...")) {
+				if (file != null)
+					filechooser.setSelectedFile(file);
+				int returnVal = filechooser.showOpenDialog(TextEditorFrame.this);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					file = filechooser.getSelectedFile();
+					openFile();
+				}
+			} else if (id.equals("Save...")) {
+				if (file != null)
+					filechooser.setSelectedFile(file);
+				int returnVal = filechooser.showSaveDialog(TextEditorFrame.this);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					file = filechooser.getSelectedFile();
+					saveFile();
+				}
+			} else if (id.equals("Exit")) {
+				System.exit(0);
+			} else if (id.equals("Cut")) {
+				text.cut();
+			} else if (id.equals("Copy")) {
+				text.copy();
+			} else if (id.equals("Paste")) {
+				text.paste();
+			} else if (id.equals("Color...")) {
+				color = JColorChooser.showDialog(TextEditorFrame.this, "", color);
+				text.setForeground(color);
+			} else if (id.equals("About")) {
+				about.setSize(100, 50);
+				about.show();
+			}
+		}
+	};
+
+	// 保存文件,将字符写入文件
+	void saveFile() {
+		try {
+			FileWriter fw = new FileWriter(file);
+			fw.write(text.getText());
+			fw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 读入文件,并将字符置入文本框中
+	void openFile() {
+		try {
+			FileReader fr = new FileReader(file);
+			int len = (int) file.length();
+			char[] buffer = new char[len];
+			fr.read(buffer, 0, len);
+			fr.close();
+			text.setText(new String(buffer));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 初始化对话框
+	void initAboutDialog() {
+		about.getContentPane().add(new JLabel("简单编辑器 V1.0"));
+		about.setModal(true);
+		about.setSize(100, 50);
+	}
+
+	// 工具条
+	JToolBar toolbar = new JToolBar();
+	JButton[] buttons = new JButton[] { new JButton("", new ImageIcon("copy.jpg")),
+			new JButton("", new ImageIcon("cut.jpg")), new JButton("", new ImageIcon("paste.jpg")) };
+
+	// 加入工具条
+	void initToolBar() {
+		for (int i = 0; i < buttons.length; ++i)
+			toolbar.add(buttons[i]);
+		buttons[0].setToolTipText("copy");
+		buttons[0].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				text.copy();
+			}
+		});
+		buttons[1].setToolTipText("cut");
+		buttons[1].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				text.cut();
+			}
+		});
+		buttons[2].setToolTipText("paste");
+		buttons[2].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				text.paste();
+			}
+		});
+		this.getContentPane().add(toolbar, BorderLayout.NORTH);
+		toolbar.setRollover(true);
+	}
+}
+
+// 应用程序
+public class TextEditorApp {
+	public static void main(String[] args) {
+		TextEditorFrame f = new TextEditorFrame();
+		f.setTitle("简单的编辑器");
+		f.setSize(400, 300);
+		f.show();
+	}
+}
